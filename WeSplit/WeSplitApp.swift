15973369e227b21699021e79f8d7e7b72cd6e81c
Rob Ranf <rob@emiyaconsulting.com>
2022-08-13 13:57:50 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rob Ranf on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
